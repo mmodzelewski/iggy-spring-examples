f39999f91dd7fa244673280f7532a73fcfae1aa8
Maciej Modzelewski <maciej@modzelewski.dev>
2026-03-17 20:14:32 +0100

    Initial commit

---
+++ iggy-producer/src/test/java/dev/modzelewski/iggy_producer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.modzelewski.iggy_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
